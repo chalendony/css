a1f9bae6bc8c682877310e12abc34cd582ea2317
Stewart <a.stewart@eos-did.com>
2019-09-14 13:58:57 +0200

    detail page links

--- selector/phineo/parser.py
+++ selector/phineo/parser.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+from string import Template
 
 from bs4 import BeautifulSoup
 import requests
@@ -9,19 +10,26 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 
+## !!! RECALL : access the element by pulling it out of the list
+
 #base = 'https://www.phineo.org/projekte?tx_phineoprojectslite_pi1%5Bpps%5D=25&tx_phineoprojectslite_pi1%5Bpointer%5D='
-home = 'https://www.phineo.org/projekte'
-class Parser:
+#home = 'https://www.phineo.org/projekte'
+
+# TODO : add logger instead of print
+PROJ_PER_PAGE = 10
+last_page = 25
+
+class Catalog:
 
     def __init__(self, log_level=logging.ERROR):
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
         self.handler = logging.StreamHandler(sys.stdout)
         self.logger.addHandler(self.handler)
-
+        self.metadata = None
 
     def pager(self):
-        self.flipBoxPages(base)
+        self.flipBoxPages()
         #turn page()
 
 
@@ -29,7 +37,6 @@ class Parser:
         selector = '#c9293 > div > div > div.marginLeft_08 > div > div.leftStyle.marginTop_16.paddingBottom_16 > div.subc.width_472.leftStyle > div:nth-child(1) > div > dl:nth-child(1) > dd > a'
         r = session.get(url)
         res =  r.html.find(selector)
-        #res = res[0].text
         return res.text
 
     def zielgruppe(self,url):
@@ -67,7 +74,6 @@ class Parser:
     def wirk_image(self,url):
         selector =  '#c9293 > div > div > div.marginLeft_08 > div > div:nth-child(5) > div.leftStyle.marginTop_12 > div:nth-child(1) > div > img'
         r = session.get(url)
-        #name = 'img[src]'
         res = r.html.find(selector)[0].attrs.get('src') # get tags's attribute
         print(f"*************** {res}")
         return res
@@ -81,17 +87,72 @@ class Parser:
         print(f"*************** {res}")
         return res
 
-    def flipBoxPages(self):
+    def list_all_pages(self):
         pages = []
-        selector = '#searchresults > div.columnTriple.leftStyle.width_704 > div.columnDouble.subc.rightStyle.width_480 > div > div > div > a'
-        r = session.get(home)
-        #name = 'a[href]'
-        res = r.html.find(selector) #.attrs.get('href')  # get tags's attribute
-        print(f"*************** {res}")
-        for e in res:
-            page = home + e.attrs.get('href')
-            print(page)
-            pages.append(page)
+
+        for i in range(0, last_page):
+            url = f"https://www.phineo.org/projekte?tx_phineoprojectslite_pi1%5Bpps%5D={PROJ_PER_PAGE}&tx_phineoprojectslite_pi1%5Bpointer%5D={i}"
+            pages.append(url)
         return pages
 
+    def get_projects(self, url):
+        #selector = '#searchresults > div:nth-child(2) > div.teaserImage.leftStyle.width_232 > a'
+        # searchresults > div:nth-child(2) > div.teaserImage.leftStyle.width_232 > a
+        # searchresults
+        lst = []
+        r = session.get(url)
+        for i in range(1,12):
+            selector = f"#searchresults > div:nth-child({i}) > div.teaserImage.leftStyle.width_232 > a"
 
+            res = r.html.find(selector)
+            if len(res) > 0:
+                ## !pull element out of list and get the elements attributes...
+                res = res[0].attrs.get('href')
+                print(f"{res}")
+                lst.append(res)
+        print(f"*********************")
+        return lst
+
+
+
+    def details(self):
+        count = 0
+        details = []
+        pages = self.list_all_pages()
+        print(f"Total pages: {len(pages)}")
+        for page in pages:
+            print(page)
+
+            num_projects_on_page = self.get_projects(page)
+            #print(f"Number Projects on Page {len(num_projects_on_page)}")
+            # for project in num_projects_on_page:
+            #     selector = f"#searchresults > div:nth-child({project}) > div.teaserText.leftStyle.width_448.marginRight_24 > a"
+            # searchresults > div.searchBox_01 > div.teaserText.leftStyle.width_448.marginRight_24 > a
+            #     r = session.get(page)
+            #     res = r.html.find(selector)
+            #     ## parser all details
+            #count = count + len(num_projects_on_page)
+
+        return  count
+
+
+    def website_project(self):
+        selector = "#c9293 > div > div > div.marginLeft_08 > div > div.leftStyle.marginTop_16.paddingBottom_16 > div.subc.width_472.leftStyle > div.subc.clearBox.defaultBox_01.leftStyle > ul > li:nth-child(3) > a"
+        pass
+
+## teaser text
+## #searchresults > div:nth-child(2) > div.teaserText.leftStyle.width_448.marginRight_24 > p
+
+
+
+  #
+  # def count_projects(self, url):
+  #       lst = []
+  #       #selector = "searchresults > div.searchBox_01 > div.teaserText.leftStyle.width_448.marginRight_24 > a"
+  #       for i in range(2, PROJ_PER_PAGE + 1):
+  #           selector = f"searchresults > div:nth-child({i}) > div.teaserText.leftStyle.width_448.marginRight_24 > a"
+  #           r = session.get(url)
+  #           res = r.html.find(selector)
+  #           print(f"Still not working : {res}")
+  #           lst.append(res)
+  #       return lst
--- selector/phineo/test/test_parser.py
+++ selector/phineo/test/test_parser.py
@@ -1,51 +1,67 @@
-from selector.phineo.parser import Parser
-url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+from selector.phineo.parser import Catalog
 
 
 def themen():
-    parser = Parser()
+    url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+    parser = Catalog()
     res = parser.themen(url)
     assert 'Gesundheit & Pflege' == res
 
 
 def test_zielgruppe():
-    parser = Parser()
+    url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+    parser = Catalog()
     res = parser.zielgruppe(url)
     assert "Kinder & Jugendliche" == res[0]
 
 
 def test_standord():
-    parser = Parser()
+    url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+    parser = Catalog()
     res = parser.standort(url)
     assert "Niedersachsen" == res
 
 
 def test_reichweite():
-    parser = Parser()
+    url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+    parser = Catalog()
     res = parser.reichweite(url)
     assert "regional" == res
 
 
 def test_auf_einen_blick():
-    parser = Parser()
+    url = 'https://www.phineo.org/projekte/details/jung-alt-begegnung-von-kindern-und-alten-menschen?tx_phineoprojectslite_pi1%5Bpointer%5D=5&tx_phineoprojectslite_pi1%5Bpps%5D=25'
+    parser = Catalog()
     res = parser.auf_einen_blick(url)
     assert "Menschen" == res[0:8]
 
 def test_leistung_image():
     url = 'https://www.phineo.org/projekte/details/dialog-demenz-im-pfaffenwinkel-demenzfreundliche-kommune'
-    parser = Parser()
+    parser = Catalog()
     res = parser.wirk_image(url)
     assert ".jpg" == res[-4 :]
 
 def test_wirk_image():
     url = 'https://www.phineo.org/projekte/details/dialog-demenz-im-pfaffenwinkel-demenzfreundliche-kommune'
-    parser = Parser()
+    parser = Catalog()
     res = parser.wirk_image(url)
     assert ".jpg" == res[-4 :]
 
+
 def test_flipBoxPages():
     url = 'https://www.phineo.org/projekte'
-    parser = Parser()
+    parser = Catalog()
     res = parser.flipBoxPages()
-    assert 5 == len(res)
+    assert len(res) == 5
 
+def test_count_details_pages():
+    url = 'https://www.phineo.org/projekte?tx_phineoprojectslite_pi1%5Bpointer%5D=2&tx_phineoprojectslite_pi1%5Bpps%5D=10'
+    parser = Catalog()
+    res = parser.count_projects(url)
+    assert len(res) == 11
+
+
+def test_count_details_pages():
+    catalog = Catalog()
+    res = catalog.details()
+    assert res == 251
